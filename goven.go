b992ce6fcea45e88046dec37377b278ba5de239d
Jahanzaibv2 <iam@ranajahanzaib.com>
2019-11-09 22:02:24 +0500

    Update

---
+++ goven.go
@@ -0,0 +1,23 @@
+/**
+ *  Appzaib  (https://appzaib.com)
+ *
+ *
+ *  @copyright      2019-present Appzaib          <info@appzaib.com>
+ *                  2019-present Rana Jahanzaib   <iam@ranajahanzaib.com>
+ *
+ *
+ *  @author         Rana Jahanzaib  <iam@ranajahanzaib.com>
+ *
+ *  @docs           https://docs.appzaib.com/goventory/goven
+ */
+
+package goven
+
+import (
+	"fmt"
+)
+
+// ImportCheck function
+func ImportCheck() {
+	fmt.Println("goven imported!")
+}
